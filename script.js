a8e9b3de94747fb89be215ec1836c91af6b43a0a
edwrddz6 <134966646+edwrddz6@users.noreply.github.com>
2023-10-31 12:10:40 -0700

    Add querySelectors

---
+++ script.js
@@ -0,0 +1,11 @@
+let heading1 = document.querySelector("h1");
+console.log(heading1);
+
+let paragraph1 = document.querySelector("p");
+console.log(paragraph1);
+
+let unorderedList = document.querySelector("ul");
+console.log(unorderedList);
+
+let lineItems = document.querySelectorAll("li");
+console.log(lineItems);
